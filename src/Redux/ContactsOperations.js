bd5a8d565f248f52b4bc912ab787a4382c41931b
AndreyKapus <kapustnikov@ukr.net>
2023-05-02 18:35:10 +0300

    reject

--- src/Redux/ContactsOperations.js
+++ src/Redux/ContactsOperations.js
@@ -15,8 +15,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchContacts',
-    async () => {
-        const contacts = await contactsApi.fetchContacts();
-        return contacts;
-    }
+    async (_, { rejectWithValue }) => {
+        try{
+            const contacts = await contactsApi.fetchContacts();
+            return contacts;
+        } catch (error) {
+        return rejectWithValue(error)
+    }}
 )
